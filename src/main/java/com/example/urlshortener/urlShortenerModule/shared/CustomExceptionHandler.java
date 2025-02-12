8c84dc69ce9d55d653a3d114b32bab5828f5c01d
muchristian <mucyochristian2@gmail.com>
2025-02-12 07:54:07 +0200

    Add Response exception handler

---
+++ src/main/java/com/example/urlshortener/urlShortenerModule/shared/CustomExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.urlshortener.urlShortenerModule.shared;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+
+    @ExceptionHandler({ResponseStatusException.class})
+    private ResponseEntity<Map<String, Object>> handleCustomException(ResponseStatusException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", ex.getStatusCode().value());
+        body.put("message", ex.getReason());
+        return new ResponseEntity<>(body, ex.getStatusCode());
+    }
+}
